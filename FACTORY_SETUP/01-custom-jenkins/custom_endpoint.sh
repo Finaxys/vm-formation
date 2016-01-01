04ac7945f9e659b9ca855d617ebf68f547eaa01a
Sedat Guclu <sguclu@finaxys.com>
2016-01-01 01:47:20 +0100

    Update custom_endpoint.sh

--- FACTORY_SETUP/01-custom-jenkins/custom_endpoint.sh
+++ FACTORY_SETUP/01-custom-jenkins/custom_endpoint.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo "STARTING DOCKER SERVICE (DOCKER IN DOCKER)"
-service docker start
+sudo service docker start
 echo "GIVING HAND TO DEFAULT ENDPOINT"
 /bin/tini -- /usr/local/bin/jenkins.sh $*
